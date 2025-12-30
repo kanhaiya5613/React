b69dbea141ebc41b5bb30afe1088f4e48dd505b1
kanhaiya kumar <kk7077574@gmail.com>
2025-12-31 02:59:56 +0530

    Add Kanhaiya component and update main rendering to include it

--- 01besicreact/src/index.js
+++ 01besicreact/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-
+import Kanhaiya from './kanhaiya';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Kanhaiya />
   </React.StrictMode>
 );
 
---
+++ 01besicreact/src/kanhaiya.js
@@ -0,0 +1,7 @@
+
+function Kanhaiya(){
+    return (
+        <h1>Hello Kanhaiya, Welcome to DOM react</h1>
+    )
+}
+export default Kanhaiya;
